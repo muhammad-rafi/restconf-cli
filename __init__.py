6cab0984e14550734a6098462372e829b4c3f60c
Muhammad Rafi <murafi@cisco.com>
2022-07-19 20:05:40 +0100

    v0.1.1

---
+++ __init__.py
@@ -0,0 +1,7 @@
+import importlib.metadata
+
+try:
+    __author__ = """Muhammad Rafi"""
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.1.1"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="restconf-cli",
-    version="0.1.0",
+    version="0.1.1",
     description='command line tool for restconf',
     author='Muhammad Rafi',
     author_email='murafi@cisco.com',
