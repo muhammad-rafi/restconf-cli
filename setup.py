fac6bbf27c353f4309ad4587320ff9ce91694179
Muhammad Rafi <murafi@cisco.com>
2022-07-20 14:07:59 +0100

    v0.1.3 Beta

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ classifiers = [
 
 setup(
     name="restconf-cli",
-    version="0.1.2",
+    version="0.1.3",
     description='command line tool for restconf',
     long_description=long_description,
     long_description_content_type='text/markdown',
